1bd2aefbba5d2419805bf9749cf223937cd13211
u-Hoshi <yuta000707@yahoo.co.jp>
2022-01-29 17:48:01 +0900

    個別タスクを見れるように実装

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -5,7 +5,10 @@ import { useDispatch } from 'react-redux'
 import { setEditedTask } from '../slices/appSlice'
 import { useMutateTask } from '../hooks/useMutateTask'
 
-const TaskItemMemo: VFC<Task> = ({ id, title, description }) => {
+const TaskItemMemo: VFC<
+  Task & { setId: React.Dispatch<React.SetStateAction<string>> }
+> = ({ id, title, description, setId }) => {
+  // TSのIntersectionを使用してTaskにsetIdの型を追加する
   const dispatch = useDispatch()
   const { deleteTaskMutation } = useMutateTask()
 
@@ -19,13 +22,19 @@ const TaskItemMemo: VFC<Task> = ({ id, title, description }) => {
     )
   }
 
+  const onClickTask = () => {
+    setId(id)
+  }
+
   const onClickTrashIcon = () => {
     deleteTaskMutation.mutate(id)
   }
 
   return (
     <li>
-      <span className="font-bold cursor-pointer">{title}</span>
+      <span className="font-bold cursor-pointer" onClick={onClickTask}>
+        {title}
+      </span>
       <div className="flex float-right ml-20">
         <PencilAltIcon
           className="h-5 w-5 mx-1 text-slate-500 cursor-pointer"
